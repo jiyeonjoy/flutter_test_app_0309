0a050a03c4b132288f6b4ee6b9844586807713e3
최지연 <choi02647@naver.com>
2023-03-12 14:36:28 +0900

    StatefulWidget 의 State 로그 찍어보기

--- lib/library/part_1.dart
+++ lib/library/part_1.dart
@@ -5,3 +5,33 @@ const String name = 'part_1';
 String getName() {
   return name;
 }
+
+void testNum() {
+  num a = 1;
+  a = 1.1;
+  int b = 2;
+}
+
+class User {
+  // const String data1 = 'data1'; // error
+  static const String data2 = 'data2'; // 가능
+
+  void some() {
+    List<int> list = List<int>.filled(3, 0);
+    var list3 = List<int>.generate(3, (i) => i*10);
+
+    list[0] = 1;
+    list.removeAt(0);
+    list.add(10);
+    list.add(100);
+
+    final String data;
+    data = "";
+    const String data3 = 'data3';
+    print(data);
+  }
+
+  final String finalA;
+  User(this.finalA);
+}
+
